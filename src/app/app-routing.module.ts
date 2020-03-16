d9ed4b1091bdbd41f1b5396826a5d7c13d47ac6e
Em Sy <em.tarroza@gmail.com>
2020-03-16 11:38:38 +1100

    Create main app, head and left-panel layout components Update app routing to add paths to dashboard, login, logout and users Import Angular Material resource modules Add link to google api fonts in index.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/login/logout.component';
+import { UsersComponent } from './components/users/users.component';
+import { AuthGuard } from './helpers/canActivateAuthGuard';
 
-
-const routes: Routes = [];
+/**
+ * dashboard: home page
+ * canActivate [AuthGuard]: will allow us to check if user is logged in, otherwise redirect to login page
+ * login: page where the user can authenticate
+ * logout: path to log the user out
+ * users: first page where we want to list the users from back end
+ */
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full', canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent},
+  { path: 'logout', component: LogoutComponent},
+  { path: 'users', component: UsersComponent,canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './layout/app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
